335d7009164f5bfd5f361f55e948806a07e5f4ae
YangLang <1004145468@qq.com>
2017-05-01 08:17:21 +0800

    创建数据库封装

---
+++ NewsCollection/Helper/DataBaseConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsCollection.Dao
+{
+    /// <summary>
+    /// 本地连接数据库配置
+    /// </summary>
+    class DataBaseConfig
+    {
+        //数据库地址
+        public static String ip = "localhost";
+        //数据库端口
+        public static int port = 3306;
+        //登录用户名
+        public static String uid = "root";
+        //登录密码
+        public static String psw = "123456";
+        //使用的数据库名
+        public static String databaseName = "testdb";
+    }
+}
---
+++ NewsCollection/Helper/DatabaseHelper.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewsCollection.Helper
+{
+    /// <summary>
+    /// 远程数据库工具类
+    /// </summary>
+    class DatabaseHelper
+    {
+        private MySqlConnection connection;
+        private DatabaseHelper() { }
+        private static DatabaseHelper mInstance;
+        public  static DatabaseHelper getInstance()
+        {
+            if(mInstance == null)
+            {
+                mInstance = new DatabaseHelper();
+            }
+            return mInstance;
+        }
+
+        public void init(String ip,String port,String uid,String psw,String database)
+        {
+            StringBuilder connStr = new StringBuilder();
+            connStr.Append("Server=").Append(ip).Append(";")
+                    .Append("Port=").Append(port).Append(";")
+                    .Append("Uid=").Append(uid).Append(";")
+                    .Append("Pwd=").Append(psw).Append(";")
+                    .Append("Database=").Append(database);
+            connection = new MySqlConnection(connStr.ToString());
+        }
+
+        public Boolean testConnect()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("连接失败");
+                return false;
+            }
+            
+            
+        }
+    }
+}
